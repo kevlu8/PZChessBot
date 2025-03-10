b115a6699dd29693e01d951d3a1d1829092e05dc
Kevin Lu <kevie.lu8@gmail.com>
2025-03-09 22:44:19 -0700

    tweak move timings again

--- engine/movetimings.hpp
+++ engine/movetimings.hpp
@@ -4,6 +4,10 @@
 
 uint64_t timemgmt(int64_t remtime, int64_t inc = 0, bool online = false) {
 	// Return time in ms that we can spend on this move
-	// If we are playing online, subtract 200 for ping
-	return std::max(1l, remtime / 25 + inc * 4 / 5 - (online ? 200 : 0));
+	if (online && remtime < 10000) {
+		// We are very low on time, just play a move
+		return 200;
+	}
+	// If we are playing online, subtract 300 for ping
+	return std::max(1l, remtime / 25 + inc * 3 / 5 - (online ? 300 : 0));
 }
