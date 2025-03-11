a9b2967fef4cd0c96bd53c0b3e7e7707dd0f9f16
Kevin Lu <kevie.lu8@gmail.com>
2025-03-10 21:34:16 -0700

    remove bot directory and use lichess-bot instead

--- engine/movetimings.hpp
+++ engine/movetimings.hpp
@@ -2,12 +2,12 @@
 
 #include "includes.hpp"
 
-uint64_t timemgmt(int64_t remtime, int64_t inc = 0, bool online = false) {
+uint64_t timemgmt(int64_t remtime, int64_t inc = 0) {
 	// Return time in ms that we can spend on this move
-	if (online && remtime < 10000) {
+	if (remtime < 10000) {
 		// We are very low on time, just play a move
 		return 200;
 	}
 	// If we are playing online, subtract 300 for ping
-	return std::max(1l, remtime / 25 + inc * 3 / 5 - (online ? 300 : 0));
+	return std::max(1l, remtime / 25 + inc * 3 / 5);
 }
