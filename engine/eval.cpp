ea08406bf9d049553dbc8158633a14054db0f119
Kevin Lu <kevie.lu8@gmail.com>
2025-04-29 22:35:27 -0700

    bencH: 175897 test removing all terms except psqt

--- engine/eval.cpp
+++ engine/eval.cpp
@@ -106,87 +106,7 @@ Value mg_eval(Board &board) {Value material = 0;
 	piecesquare += pawn_acc + knight_acc + bishop_acc + rook_acc + queen_acc + king_acc;
 	piecesquare -= pawn_acc_black + knight_acc_black + bishop_acc_black + rook_acc_black + queen_acc_black + king_acc_black;
 
-	castling += (board.castling & WHITE_OO) ? 5 : 0;
-	castling += (board.castling & WHITE_OOO) ? 5 : 0;
-	castling -= (board.castling & BLACK_OO) ? 5 : 0;
-	castling -= (board.castling & BLACK_OOO) ? 5 : 0;
-
-	bishop_pair += _mm_popcnt_u64(board.piece_boards[BISHOP] & board.piece_boards[OCC(WHITE)]) >= 2 ? 30 : 0;
-	bishop_pair -= _mm_popcnt_u64(board.piece_boards[BISHOP] & board.piece_boards[OCC(BLACK)]) >= 2 ? 30 : 0;
-
-	// For king safety, check for opponent control on squares around the king
-	// As well as counting our own pieces in front of the king
-
-	king_safety += king_safety_lookup[_mm_popcnt_u64(
-		king_movetable[__tzcnt_u64(board.piece_boards[KING] & board.piece_boards[OCC(WHITE)])] & board.piece_boards[OCC(WHITE)]
-	)];
-	king_safety -= king_safety_lookup[_mm_popcnt_u64(
-		king_movetable[__tzcnt_u64(board.piece_boards[KING] & board.piece_boards[OCC(BLACK)])] & board.piece_boards[OCC(BLACK)]
-	)];
-	Bitboard white_king = (board.piece_boards[KING] & board.piece_boards[OCC(WHITE)]);
-	Bitboard black_king = (board.piece_boards[KING] & board.piece_boards[OCC(BLACK)]);
-	if (white_king & (square_bits(SQ_G1) | square_bits(SQ_H1))) {
-		std::pair<int, int> control = board.control(SQ_F2);
-		king_safety -= std::max(0, control.second - control.first) * 10;
-		control = board.control(SQ_G2);
-		king_safety -= std::max(0, control.second - control.first) * 15;
-		control = board.control(SQ_H2);
-		king_safety -= std::max(0, control.second - control.first) * 15;
-	} else if (white_king & (square_bits(SQ_B1) | square_bits(SQ_C1))) {
-		std::pair<int, int> control = board.control(SQ_A2);
-		king_safety -= std::max(0, control.second - control.first) * 12;
-		control = board.control(SQ_B2);
-		king_safety -= std::max(0, control.second - control.first) * 15;
-		control = board.control(SQ_C2);
-		king_safety -= std::max(0, control.second - control.first) * 15;
-	}
-
-	if (black_king & (square_bits(SQ_G8) | square_bits(SQ_H8))) {
-		std::pair<int, int> control = board.control(SQ_F7);
-		king_safety += std::max(0, control.first - control.second) * 10;
-		control = board.control(SQ_G7);
-		king_safety += std::max(0, control.first - control.second) * 15;
-		control = board.control(SQ_H7);
-		king_safety += std::max(0, control.first - control.second) * 15;
-	} else if (black_king & (square_bits(SQ_B8) | square_bits(SQ_C8))) {
-		std::pair<int, int> control = board.control(SQ_A7);
-		king_safety += std::max(0, control.first - control.second) * 12;
-		control = board.control(SQ_B7);
-		king_safety += std::max(0, control.first - control.second) * 15;
-		control = board.control(SQ_C7);
-		king_safety += std::max(0, control.first - control.second) * 15;
-	}
-
-	tempo_bonus += board.side == WHITE ? 10 : -10;
-
-	for (Bitboard mask = 0x0101010101010101; mask & 0xff; mask <<= 1) {
-		// Doubled pawns
-		pawn_structure -= multipawn_lookup[_mm_popcnt_u64(board.piece_boards[PAWN] & board.piece_boards[OCC(WHITE)] & mask)];
-		pawn_structure += multipawn_lookup[_mm_popcnt_u64(board.piece_boards[PAWN] & board.piece_boards[OCC(BLACK)] & mask)];
-
-		// Isolated pawns
-		if (mask & 0b01111110) {
-			if ((board.piece_boards[PAWN] & board.piece_boards[OCC(WHITE)] & ((mask << 1) | (mask >> 1))) == 0)
-				pawn_structure -= _mm_popcnt_u64(board.piece_boards[PAWN] & board.piece_boards[OCC(WHITE)] & mask) ? 60 : 0;
-			if ((board.piece_boards[PAWN] & board.piece_boards[OCC(BLACK)] & ((mask << 1) | (mask >> 1))) == 0)
-				pawn_structure += _mm_popcnt_u64(board.piece_boards[PAWN] & board.piece_boards[OCC(BLACK)] & mask) ? 60 : 0;
-		}
-	}
-
-	Bitboard pawns = board.piece_boards[PAWN] & board.piece_boards[OCC(WHITE)];
-	while (pawns) {
-		int sq = _tzcnt_u64(pawns);
-		pawn_structure += (board.piece_boards[PAWN] & PASSED_PAWN_MASKS[WHITE][sq]) == 0 ? 80 : 0;
-		pawns = _blsr_u64(pawns);
-	}
-	pawns = board.piece_boards[PAWN] & board.piece_boards[OCC(BLACK)];
-	while (pawns) {
-		int sq = _tzcnt_u64(pawns);
-		pawn_structure -= (board.piece_boards[PAWN] & PASSED_PAWN_MASKS[BLACK][sq]) == 0 ? 80 : 0;
-		pawns = _blsr_u64(pawns);
-	}
-
-	return ((int)material + (int)piecesquare + (int)castling + (int)bishop_pair + (int)king_safety + (int)tempo_bonus + (int)pawn_structure);
+	return ((int)material + (int)piecesquare);
 }
 
 Value eg_eval(Board &board) {
@@ -237,34 +157,7 @@ Value eg_eval(Board &board) {
 	piecesquare += pawn_acc + knight_acc + bishop_acc + rook_acc + queen_acc;
 	piecesquare -= pawn_acc_black + knight_acc_black + bishop_acc_black + rook_acc_black + queen_acc_black;
 
-	for (Bitboard mask = 0x0101010101010101; mask & 0xff; mask <<= 1) {
-		// Doubled pawns
-		pawn_structure -= multipawn_lookup[_mm_popcnt_u64(board.piece_boards[PAWN] & board.piece_boards[OCC(WHITE)] & mask)];
-		pawn_structure += multipawn_lookup[_mm_popcnt_u64(board.piece_boards[PAWN] & board.piece_boards[OCC(BLACK)] & mask)];
-
-		// Isolated pawns
-		if (mask & 0b01111110) {
-			if ((board.piece_boards[PAWN] & board.piece_boards[OCC(WHITE)] & ((mask << 1) | (mask >> 1))) == 0)
-				pawn_structure -= _mm_popcnt_u64(board.piece_boards[PAWN] & board.piece_boards[OCC(WHITE)] & mask) ? 60 : 0;
-			if ((board.piece_boards[PAWN] & board.piece_boards[OCC(BLACK)] & ((mask << 1) | (mask >> 1))) == 0)
-				pawn_structure += _mm_popcnt_u64(board.piece_boards[PAWN] & board.piece_boards[OCC(BLACK)] & mask) ? 60 : 0;
-		}
-	}
-
-	Bitboard pawns = board.piece_boards[PAWN] & board.piece_boards[OCC(WHITE)];
-	while (pawns) {
-		int sq = _tzcnt_u64(pawns);
-		pawn_structure += (board.piece_boards[PAWN] & PASSED_PAWN_MASKS[WHITE][sq]) == 0 ? 80 : 0;
-		pawns = _blsr_u64(pawns);
-	}
-	pawns = board.piece_boards[PAWN] & board.piece_boards[OCC(BLACK)];
-	while (pawns) {
-		int sq = _tzcnt_u64(pawns);
-		pawn_structure -= (board.piece_boards[PAWN] & PASSED_PAWN_MASKS[BLACK][sq]) == 0 ? 80 : 0;
-		pawns = _blsr_u64(pawns);
-	}
-
-	return ((int)material + (int)piecesquare + (int)pawn_structure);
+	return ((int)material + (int)piecesquare);
 }
 
 Value eval(Board &board) {
