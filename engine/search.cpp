ca750f8414612f5e6b4fc22141b416dd199b1698
Kevin Lu <kevie.lu8@gmail.com>
2025-07-26 08:19:37 -0700

    Make LMP less aggressive
    Bench: 142778

--- engine/search.cpp
+++ engine/search.cpp
@@ -425,7 +425,7 @@ Value __recurse(Board &board, int depth, Value alpha = -VALUE_INFINITE, Value be
 		bool capt = (board.piece_boards[OPPOCC(board.side)] & square_bits(move.dst()));
 		bool promo = (move.type() == PROMOTION);
 
-		if (!pv && i > 5 + depth * depth && !in_check && !capt && !promo && abs(alpha) < VALUE_MATE_MAX_PLY && abs(beta) < VALUE_MATE_MAX_PLY) {
+		if (!pv && i > 8 + depth * depth && !in_check && !capt && !promo && abs(alpha) < VALUE_MATE_MAX_PLY && abs(beta) < VALUE_MATE_MAX_PLY) {
 			/**
 			 * Late move pruning
 			 * 
