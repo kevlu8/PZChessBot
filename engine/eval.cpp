c32a792a01d2cf911f15ea99187c0b02ea2cdc52
Kevin Lu <kevie.lu8@gmail.com>
2025-08-10 23:32:11 -0700

    Taper pawn and king in endgame
    Bench: 684412

--- engine/eval.cpp
+++ engine/eval.cpp
@@ -84,6 +84,9 @@ Value eval(Board &board) {
 	Value tempo_bonus = 0;
 	Value pawn_structure = 0;
 
+	Value phase = _mm_popcnt_u64(board.piece_boards[OCC(WHITE)] | board.piece_boards[OCC(BLACK)]);
+	Value eg_weight = phase <= 16 ? 20 - phase : 0;
+
 	material += PawnValue * _mm_popcnt_u64(board.piece_boards[PAWN] & board.piece_boards[OCC(WHITE)]);
 	material += KnightValue * _mm_popcnt_u64(board.piece_boards[KNIGHT] & board.piece_boards[OCC(WHITE)]);
 	material += BishopValue * _mm_popcnt_u64(board.piece_boards[BISHOP] & board.piece_boards[OCC(WHITE)]);
@@ -102,12 +105,12 @@ Value eval(Board &board) {
 		PieceType pt = (PieceType)(p & 7);
 		int idx = side == 1 ? i : (56 ^ i);
 		switch (pt) {
-			case PAWN: piecesquare += side * pawn_heatmap[idx]; break;
+			case PAWN: piecesquare += side * ((pawn_heatmap[idx] * (24 - eg_weight)) + (pawn_endgame[idx] * eg_weight)) / 24; break;
 			case KNIGHT: piecesquare += side * knight_heatmap[idx]; break;
 			case BISHOP: piecesquare += side * bishop_heatmap[idx]; break;
 			case ROOK: piecesquare += side * rook_heatmap[idx]; break;
 			case QUEEN: piecesquare += side * queen_heatmap[idx]; break;
-			case KING: piecesquare += side * king_heatmap[idx]; break;
+			case KING: piecesquare += side * ((king_heatmap[idx] * (24 - eg_weight)) + (endgame_heatmap[idx] * eg_weight)) / 24; break;
 		}
 	}
 
