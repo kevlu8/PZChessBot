06450a88720461849ca2a7206d047f55f5ef0ac9
Kevin Lu <kevie.lu8@gmail.com>
2025-08-12 18:55:40 -0700

    Change RFP threshold to 100
    Bench: 549005

--- engine/search.hpp
+++ engine/search.hpp
@@ -11,7 +11,7 @@
 // we can afford to lose RFP_THRESHOLD eval units per ply
 // and still be in a better position. The lower the value,
 // the more aggressive RFP is.
-#define RFP_THRESHOLD (131 * CP_SCALE_FACTOR)
+#define RFP_THRESHOLD (100 * CP_SCALE_FACTOR)
 #define RFP_IMPROVING (30 * CP_SCALE_FACTOR)
 
 // Aspiration window size(s)
