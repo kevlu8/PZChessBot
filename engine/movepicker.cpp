453910bc0ca78e2bce53318e86c62d6852728afb
Kevin Lu <kevie.lu8@gmail.com>
2025-10-01 18:11:55 -0700

    Change staged movegen order
    Bench: 11119997

--- engine/movepicker.cpp
+++ engine/movepicker.cpp
@@ -10,31 +10,14 @@ Move MovePicker::next() {
 		return NullMove;
 
 	if (stage == MP_STAGE_TT) {
-		stage = MP_STAGE_KILLER1;
+		stage = MP_STAGE_GEN;
 		if (ttMove != NullMove && board.is_pseudolegal(ttMove))
 			return ttMove;
 	}
 
-	if (stage == MP_STAGE_KILLER1) {
-		stage = MP_STAGE_KILLER2;
-		if (ss->killer[0] != NullMove && ss->killer[0] != ttMove && board.is_pseudolegal(ss->killer[0]))
-			return ss->killer[0];
-	}
-
-	if (stage == MP_STAGE_KILLER2) {
-		stage = MP_STAGE_GEN;
-		if (ss->killer[1] != NullMove && ss->killer[1] != ttMove && ss->killer[1] != ss->killer[0] && board.is_pseudolegal(ss->killer[1]))
-			return ss->killer[1];
-	}
-
 	if (stage == MP_STAGE_GEN) {
-		stage = MP_STAGE_SCORE;
+		stage = MP_STAGE_NOISY;
 		board.legal_moves(moves);
-		end = moves.size();
-	}
-
-	if (stage == MP_STAGE_SCORE) {
-		stage = MP_STAGE_MOVES;
 
 		const int CAPTURE_PROMO_BASE = 1000000;
 		const int QUIET_BASE = 0;
@@ -56,13 +39,54 @@ Move MovePicker::next() {
 				if (promo)
 					score += PieceValue[move.promotion() + KNIGHT] - PawnValue;
 			} else {
+				if (qskip) continue;
 				score = QUIET_BASE + main_hist->get_history(board, move, ply, ss);
 			}
-			scores.push_back(std::make_pair(move, score));
+			scores.push_back({move, score});
 		}
+
+		end = scores.size();
 	}
 
-	if (stage == MP_STAGE_MOVES) {
+	if (stage == MP_STAGE_NOISY) {
+		Move best_move = NullMove;
+		int best_score = -2147483647;
+		int idx = 0;
+		for (int i = 0; i < end; i++) {
+			if (!board.is_capture(scores[i].first) && scores[i].first.type() != PROMOTION)
+				continue;
+			if (scores[i].second > best_score) {
+				best_score = scores[i].second;
+				best_move = scores[i].first;
+				idx = i;
+			}
+		}
+		std::swap(scores[idx], scores[end - 1]);
+		end--;
+		if (end == 0) stage = MP_STAGE_KILLER1;
+		if (best_move != NullMove || qskip)
+			return best_move;
+		else stage = MP_STAGE_KILLER1;
+	}
+
+	if (qskip) {
+		stage = MP_STAGE_DONE;
+		return NullMove;
+	}
+
+	if (stage == MP_STAGE_KILLER1) {
+		stage = MP_STAGE_KILLER2;
+		if (ss->killer[0] != NullMove && ss->killer[0] != ttMove && board.is_pseudolegal(ss->killer[0]))
+			return ss->killer[0];
+	}
+
+	if (stage == MP_STAGE_KILLER2) {
+		stage = MP_STAGE_QUIETS;
+		if (ss->killer[1] != NullMove && ss->killer[1] != ttMove && ss->killer[1] != ss->killer[0] && board.is_pseudolegal(ss->killer[1]))
+			return ss->killer[1];
+	}
+
+	if (stage == MP_STAGE_QUIETS) {
 		if (end == 0) {
 			stage = MP_STAGE_DONE;
 			return NullMove;
@@ -87,5 +111,6 @@ Move MovePicker::next() {
 		return NullMove;
 	}
 
-	throw std::runtime_error("Invalid MovePicker stage");
+	std::cout << "Invalid MovePicker stage: " << stage << std::endl;
+	throw std::runtime_error("Il faut que nous mourions");
 }
