6b1d9ba348c46250439c10e19a71ea87b695dbcc
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-11-04 08:04:32 +0100

    make SEE pruning more aggro
    bench: 13079420

--- engine/search.cpp
+++ engine/search.cpp
@@ -474,7 +474,7 @@ Value __recurse(Board &board, int depth, Value alpha = -VALUE_INFINITE, Value be
 				 * Skip searching moves with bad SEE scores
 				 */
 				Value see = board.see_capture(move);
-				if (see < (-100 - 100 * capt) * depth)
+				if (see < (-50 - 50 * capt) * depth)
 					continue;
 			}
 		}
