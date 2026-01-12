5a15ff88ea399d8e2293d1205a09347dfe037088
William Ma <wdotmathree@gmail.com>
2026-01-12 10:19:12 -0500

    Add thread pooling
    Bench: 4173371

--- engine/main.cpp
+++ engine/main.cpp
@@ -20,6 +20,20 @@ bool quiet = false, online = false, dfrc_uci = false;
 
 ThreadInfo *tis;
 
+void regenerate_threads() {
+	kill_threads();
+	threads.clear();
+	delete[] tis;
+	tis = new ThreadInfo[num_threads];
+	threads.reserve(num_threads);
+	for (int i = 0; i < num_threads; i++) {
+		tis[i].is_main = (i == 0);
+		tis[i].id = i;
+		tis[i].set_bs();
+		threads.emplace_back(thread_loop, std::ref(tis[i]));
+	}
+}
+
 void run_uci() {
 	std::thread searchthread;
 	std::string command;
@@ -34,6 +48,7 @@ void run_uci() {
 			std::cout << "option name UCI_Chess960 type check default false" << std::endl;
 			std::cout << "uciok" << std::endl;
 		} else if (command == "icu") {
+			kill_threads();
 			return; // exit uci mode
 		} else if (command == "isready") {
 			std::cout << "readyok" << std::endl;
@@ -65,16 +80,15 @@ void run_uci() {
 					std::cerr << "Invalid number of threads: " << num_threads << std::endl;
 					num_threads = 1;
 				}
-				delete[] tis;
-				tis = new ThreadInfo[num_threads];
-				for (int i = 0; i < num_threads; i++) tis[i].set_bs();
+				regenerate_threads();
 				std::cout << "info string Using " << num_threads << " threads" << std::endl;
 			} else if (optionname == "UCI_Chess960") {
 				dfrc_uci = (optionvalue == "true");
 			}
 		} else if (command == "ucinewgame") {
-			stop_search = true;
+			regenerate_threads();
 			if (searchthread.joinable()) searchthread.join();
+
 			board = Board();
 			ttable.resize(TT_SIZE);
 			shared_corrhist = Corrhist();
@@ -101,6 +115,7 @@ void run_uci() {
 				}
 			}
 		} else if (command == "quit") {
+			kill_threads();
 			if (searchthread.joinable()) searchthread.join();
 			exit(0);
 		} else if (command == "stop") {
@@ -190,7 +205,10 @@ void run_uci() {
 
 __attribute__((weak)) int main(int argc, char *argv[]) {
 	tis = new ThreadInfo[1]; // single thread for now
+	tis[0].is_main = true;
+	tis[0].id = 0;
 	tis[0].set_bs();
+	threads.emplace_back(thread_loop, std::ref(tis[0]));
 	if (argc == 2 && std::string(argv[1]) == "bench") {
 		const std::string bench_positions[] = {
 			"r3k2r/2pb1ppp/2pp1q2/p7/1nP1B3/1P2P3/P2N1PPP/R2QK2R w KQkq - 0 14",
@@ -255,6 +273,7 @@ __attribute__((weak)) int main(int argc, char *argv[]) {
 		}
 		uint64_t end = clock();
 		std::cout << tot_nodes << " nodes " << int(tot_nodes / ((double)(end - start) / CLOCKS_PER_SEC)) << " nps" << std::endl;
+		kill_threads();
 		return 0;
 	}
 	if (argc == 3 && std::string(argv[2]) == "quit") {
