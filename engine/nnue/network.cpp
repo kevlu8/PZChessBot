366412b18f0b0cc9dc6ceab66e8d01a0756b4356
William Ma <wdotmathree@gmail.com>
2026-01-31 19:00:18 -0500

    Remove NNUE extraneous computation
    Bench: 5113271

--- engine/nnue/network.cpp
+++ engine/nnue/network.cpp
@@ -61,38 +61,21 @@ int32_t nnue_eval(const Network &net, const Accumulator &stm, const Accumulator 
 		__m256i stm_weights = _mm256_loadu_si256((__m256i*)&net.output_weights[nbucket][i]);
 		__m256i ntm_weights = _mm256_loadu_si256((__m256i*)&net.output_weights[nbucket][HL_SIZE + i]);
 
-		__m256i stm_prod_lo = _mm256_mullo_epi16(stm_vals, stm_weights);
-		__m256i stm_prod_hi = _mm256_mulhi_epi16(stm_vals, stm_weights);
+		__m256i stm_prod = _mm256_mullo_epi16(stm_vals, stm_weights);
+		__m256i ntm_prod = _mm256_mullo_epi16(ntm_vals, ntm_weights);
 
-		__m256i ntm_prod_lo = _mm256_mullo_epi16(ntm_vals, ntm_weights);
-		__m256i ntm_prod_hi = _mm256_mulhi_epi16(ntm_vals, ntm_weights);
+		__m256i stm_res = _mm256_madd_epi16(stm_prod, stm_vals);
+		__m256i ntm_res = _mm256_madd_epi16(ntm_prod, ntm_vals);
 
-		__m256i stm_lo_32 = _mm256_unpacklo_epi16(stm_prod_lo, stm_prod_hi);
-		__m256i stm_hi_32 = _mm256_unpackhi_epi16(stm_prod_lo, stm_prod_hi);
-		__m256i ntm_lo_32 = _mm256_unpacklo_epi16(ntm_prod_lo, ntm_prod_hi);
-		__m256i ntm_hi_32 = _mm256_unpackhi_epi16(ntm_prod_lo, ntm_prod_hi);
-
-		__m256i stm_vals_lo = _mm256_unpacklo_epi16(stm_vals, zero);
-		__m256i stm_vals_hi = _mm256_unpackhi_epi16(stm_vals, zero);
-		__m256i ntm_vals_lo = _mm256_unpacklo_epi16(ntm_vals, zero);
-		__m256i ntm_vals_hi = _mm256_unpackhi_epi16(ntm_vals, zero);
-
-		stm_lo_32 = _mm256_mullo_epi32(stm_lo_32, stm_vals_lo);
-		stm_hi_32 = _mm256_mullo_epi32(stm_hi_32, stm_vals_hi);
-		ntm_lo_32 = _mm256_mullo_epi32(ntm_lo_32, ntm_vals_lo);
-		ntm_hi_32 = _mm256_mullo_epi32(ntm_hi_32, ntm_vals_hi);
-
-		sum = _mm256_add_epi32(sum, stm_lo_32);
-		sum = _mm256_add_epi32(sum, stm_hi_32);
-		sum = _mm256_add_epi32(sum, ntm_lo_32);
-		sum = _mm256_add_epi32(sum, ntm_hi_32);
+		sum = _mm256_add_epi32(sum, stm_res);
+		sum = _mm256_add_epi32(sum, ntm_res);
 	}
 
 	__m128i sum_128 = _mm_add_epi32(_mm256_extracti128_si256(sum, 0), _mm256_extracti128_si256(sum, 1));
 	sum_128 = _mm_add_epi32(sum_128, _mm_shuffle_epi32(sum_128, _MM_SHUFFLE(2, 3, 0, 1)));
 	sum_128 = _mm_add_epi32(sum_128, _mm_shuffle_epi32(sum_128, _MM_SHUFFLE(1, 0, 3, 2)));
 	int32_t score = _mm_cvtsi128_si32(sum_128);
-	
+
 	score /= QA;
 	score += net.output_bias[nbucket];
 	score *= SCALE;
