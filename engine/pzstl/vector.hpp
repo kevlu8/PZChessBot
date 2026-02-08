06b70750c0944bbd56c95f94571ce5443989c6c9
William Ma <wdotmathree@gmail.com>
2026-02-07 21:09:53 -0500

    Fix play past threefold (#262)
    ```
    Elo   | 3.35 +- 4.02 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | 2.90 (-2.25, 2.89) [-5.00, 0.00]
    Games | N: 7894 W: 1843 L: 1767 D: 4284
    Penta | [28, 929, 1965, 989, 36]
    ```
    https://ob.int0x80.ca/test/261/
    
    Bench: 5092271

--- engine/pzstl/vector.hpp
+++ engine/pzstl/vector.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-#define PZSTL_MAX_SIZE 256
+#define PZSTD_DEFAULT_SIZE 256
 
 namespace pzstd {
-	template <typename T>
+	template <typename T, size_t MaxSize = PZSTD_DEFAULT_SIZE>
 	struct vector {
 		// It is necessary to write this class using static allocation only
-		T data[PZSTL_MAX_SIZE];
+		T data[MaxSize];
 		uint16_t sz = 0;
 
 		void push_back(T value) noexcept {
@@ -26,50 +26,6 @@ namespace pzstd {
 				}
 			}
 			return cnt;
-		}
-		T &operator[](uint16_t index) {
-			return data[index];
-		}
-		const T &operator[](uint16_t index) const {
-			return data[index];
-		}
-		uint16_t size() const {
-			return sz;
-		}
-		bool empty() const {
-			return sz == 0;
-		}
-
-		// Iterator methods
-		T * __restrict__ begin() {
-			return data;
-		}
-		T * __restrict__ end() {
-			return data + sz;
-		}
-		const T * __restrict__ begin() const {
-			return data;
-		}
-		const T * __restrict__ end() const {
-			return data + sz;
-		}
-	};
-
-	// This struct is (temporarily) necessary for storing hash_hist as games may last longer than 256 ply.
-	// In theory, once hash_hist is implemented properly, it will never exceed size 50 due to the 50 move rule.
-	template<typename T>
-	struct largevector {
-		T data[1024];
-		uint16_t sz = 0;
-
-		void push_back(T value) noexcept {
-			data[sz++] = value;
-		}
-		void pop_back() noexcept {
-			sz--;
-		}
-		void clear() {
-			sz = 0;
 		}
 		T &operator[](uint16_t index) {
 			return data[index];
