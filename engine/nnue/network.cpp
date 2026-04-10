670edc063f6318f5ad71886699d323efd6836b60
William Ma <wdotmathree@gmail.com>
2026-04-10 01:35:39 -0400

    Do pairwise activation and upgrade to PZNet42 (#303)
    ```
    Elo   | 1.06 +- 2.96 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | 2.89 (-2.25, 2.89) [-5.00, 0.00]
    Games | N: 15674 W: 3941 L: 3893 D: 7840
    Penta | [89, 1928, 3765, 1956, 99]
    ```
    https://ob.int0x80.ca/test/634/
    
    Bench: 3851225
    
    ---------
    
    Co-authored-by: Kevin Lu <kevie.lu8@gmail.com>

--- engine/nnue/network.cpp
+++ engine/nnue/network.cpp
@@ -34,24 +34,30 @@ int32_t nnue_eval(const Network &net, const Accumulator &stm, const Accumulator 
 	const __m256i zero = _mm256_setzero_si256();
 	const __m256i qa_vec = _mm256_set1_epi16(QA);
 
-	for (int i = 0; i < HL_SIZE; i += 16) {
-		__m256i stm_vals = _mm256_loadu_si256((__m256i*)&stm.val[i]);
-		__m256i ntm_vals = _mm256_loadu_si256((__m256i*)&ntm.val[i]);
+	for (int i = 0; i < HL_SIZE / 2; i += 16) {
+		__m256i stm_vals1 = _mm256_loadu_si256((__m256i *)&stm.val[i]);
+		__m256i stm_vals2 = _mm256_loadu_si256((__m256i *)&stm.val[i + HL_SIZE / 2]);
+		__m256i ntm_vals1 = _mm256_loadu_si256((__m256i *)&ntm.val[i]);
+		__m256i ntm_vals2 = _mm256_loadu_si256((__m256i *)&ntm.val[i + HL_SIZE / 2]);
 
-		stm_vals = _mm256_max_epi16(stm_vals, zero);
-		stm_vals = _mm256_min_epi16(stm_vals, qa_vec);
+		stm_vals1 = _mm256_max_epi16(stm_vals1, zero);
+		stm_vals1 = _mm256_min_epi16(stm_vals1, qa_vec);
+		stm_vals2 = _mm256_max_epi16(stm_vals2, zero);
+		stm_vals2 = _mm256_min_epi16(stm_vals2, qa_vec);
 
-		ntm_vals = _mm256_max_epi16(ntm_vals, zero);
-		ntm_vals = _mm256_min_epi16(ntm_vals, qa_vec);
+		ntm_vals1 = _mm256_max_epi16(ntm_vals1, zero);
+		ntm_vals1 = _mm256_min_epi16(ntm_vals1, qa_vec);
+		ntm_vals2 = _mm256_max_epi16(ntm_vals2, zero);
+		ntm_vals2 = _mm256_min_epi16(ntm_vals2, qa_vec);
 
-		__m256i stm_weights = _mm256_loadu_si256((__m256i*)&net.output_weights[nbucket][i]);
-		__m256i ntm_weights = _mm256_loadu_si256((__m256i*)&net.output_weights[nbucket][HL_SIZE + i]);
+		__m256i stm_weights = _mm256_loadu_si256((__m256i *)&net.output_weights[nbucket][i]);
+		__m256i ntm_weights = _mm256_loadu_si256((__m256i *)&net.output_weights[nbucket][i + HL_SIZE / 2]);
 
-		__m256i stm_prod = _mm256_mullo_epi16(stm_vals, stm_weights);
-		__m256i ntm_prod = _mm256_mullo_epi16(ntm_vals, ntm_weights);
+		__m256i stm_prod = _mm256_mullo_epi16(stm_vals1, stm_weights);
+		__m256i ntm_prod = _mm256_mullo_epi16(ntm_vals1, ntm_weights);
 
-		__m256i stm_res = _mm256_madd_epi16(stm_prod, stm_vals);
-		__m256i ntm_res = _mm256_madd_epi16(ntm_prod, ntm_vals);
+		__m256i stm_res = _mm256_madd_epi16(stm_prod, stm_vals2);
+		__m256i ntm_res = _mm256_madd_epi16(ntm_prod, ntm_vals2);
 
 		sum = _mm256_add_epi32(sum, stm_res);
 		sum = _mm256_add_epi32(sum, ntm_res);
