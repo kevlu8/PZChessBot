7b8d4b47cd194e6aa8fd63feab680b9eca7a6d43
William Ma <wdotmathree@gmail.com>
2026-04-09 21:53:26 -0400

    Vectorize
    Bench: 3609107

--- engine/nnue/network.cpp
+++ engine/nnue/network.cpp
@@ -50,57 +50,97 @@ int32_t nnue_eval(const Network &net, const Accumulator &stm, const Accumulator 
 	float l2[L2_SIZE];
 
 	for (int i = 0; i < L1_SIZE; i++) {
-		int32_t sum = 0;
+		__m256i sum = _mm256_setzero_si256();
+		__m256i zero = _mm256_setzero_si256();
+		__m256i clip = _mm256_set1_epi16(QA);
 
-		for (int j = 0; j < L0_SIZE / 2; j++) {
-			int16_t stm_val1 = stm.val[j];
-			int16_t stm_val2 = stm.val[j + L0_SIZE / 2];
-			int16_t ntm_val1 = ntm.val[j];
-			int16_t ntm_val2 = ntm.val[j + L0_SIZE / 2];
+		for (int j = 0; j < L0_SIZE / 2; j += 16) {
+			__m256i stm_val1 = _mm256_load_si256((__m256i *)&stm.val[j]);
+			__m256i stm_val2 = _mm256_load_si256((__m256i *)&stm.val[j + L0_SIZE / 2]);
+			__m256i ntm_val1 = _mm256_load_si256((__m256i *)&ntm.val[j]);
+			__m256i ntm_val2 = _mm256_load_si256((__m256i *)&ntm.val[j + L0_SIZE / 2]);
 
-			stm_val1 = std::clamp(stm_val1, (int16_t)0, (int16_t)QA);
-			stm_val2 = std::clamp(stm_val2, (int16_t)0, (int16_t)QA);
-			ntm_val1 = std::clamp(ntm_val1, (int16_t)0, (int16_t)QA);
-			ntm_val2 = std::clamp(ntm_val2, (int16_t)0, (int16_t)QA);
+			stm_val1 = _mm256_max_epi16(stm_val1, zero);
+			stm_val1 = _mm256_min_epi16(stm_val1, clip);
 
-			int32_t stm_weight = net.l1_weights[nbucket][i][j];
-			int32_t ntm_weight = net.l1_weights[nbucket][i][j + L0_SIZE / 2];
+			stm_val2 = _mm256_max_epi16(stm_val2, zero);
+			stm_val2 = _mm256_min_epi16(stm_val2, clip);
 
-			sum += stm_weight * stm_val1 * stm_val2;
-			sum += ntm_weight * ntm_val1 * ntm_val2;
+			ntm_val1 = _mm256_max_epi16(ntm_val1, zero);
+			ntm_val1 = _mm256_min_epi16(ntm_val1, clip);
+
+			ntm_val2 = _mm256_max_epi16(ntm_val2, zero);
+			ntm_val2 = _mm256_min_epi16(ntm_val2, clip);
+
+			__m256i stm_weight = _mm256_cvtepi8_epi16(_mm_load_si128((__m128i *)&net.l1_weights[nbucket][i][j]));
+			__m256i ntm_weight = _mm256_cvtepi8_epi16(_mm_load_si128((__m128i *)&net.l1_weights[nbucket][i][j + L0_SIZE / 2]));
+
+			__m256i stm_prod = _mm256_mullo_epi16(stm_val1, stm_weight);
+			__m256i ntm_prod = _mm256_mullo_epi16(ntm_val1, ntm_weight);
+
+			__m256i stm_res = _mm256_madd_epi16(stm_val2, stm_prod);
+			__m256i ntm_res = _mm256_madd_epi16(ntm_val2, ntm_prod);
+
+			sum = _mm256_add_epi32(sum, stm_res);
+			sum = _mm256_add_epi32(sum, ntm_res);
 		}
 
-		float f_sum = sum;
-		f_sum /= QA * QA * QB;
-		f_sum += net.l1_biases[nbucket][i];
+		__m128i sum_128 = _mm_add_epi32(_mm256_castsi256_si128(sum), _mm256_extracti128_si256(sum, 1));
+		sum_128 = _mm_add_epi32(sum_128, _mm_shuffle_epi32(sum_128, _MM_SHUFFLE(3, 3, 1, 1)));
+		sum_128 = _mm_add_epi32(sum_128, _mm_shuffle_epi32(sum_128, _MM_SHUFFLE(3, 2, 3, 2)));
 
-		l1[i] = f_sum;
+		// l1 is secretly an array of int32
+		int32_t f_sum = _mm_cvtsi128_si32(sum_128);
+		l1[i] = std::bit_cast<float, int32_t>(f_sum);
+	}
+
+	// Convert l1 into a proper float array
+	__m256 div = _mm256_set1_ps(1.0f / (QA * QA * QB));
+	for (int i = 0; i < L1_SIZE; i += 8) {
+		__m256i i_val = _mm256_load_si256((__m256i *)&l1[i]);
+		__m256 f_val = _mm256_cvtepi32_ps(i_val);
+
+		__m256 bias = _mm256_load_ps(&net.l1_biases[nbucket][i]);
+
+		f_val = _mm256_fmadd_ps(f_val, div, bias);
+
+		_mm256_store_ps(&l1[i], f_val);
 	}
 
 	for (int i = 0; i < L2_SIZE; i++) {
-		float sum = net.l2_biases[nbucket][i];
+		__m256 sum = _mm256_setzero_ps();
+		__m256 zero = _mm256_setzero_ps();
+		__m256 clip = _mm256_set1_ps(1.0f);
 
-		for (int j = 0; j < L1_SIZE; j++) {
-			float val = l1[j];
-			val = std::clamp(val, 0.0f, 1.0f);
+		for (int j = 0; j < L1_SIZE; j += 8) {
+			__m256 val = _mm256_load_ps(&l1[j]);
 
-			float weight = net.l2_weights[nbucket][i][j];
+			val = _mm256_max_ps(val, zero);
+			val = _mm256_min_ps(val, clip);
 
-			sum += val * val * weight;
+			__m256 weight = _mm256_load_ps(&net.l2_weights[nbucket][i][j]);
+
+			__m256 res = _mm256_mul_ps(_mm256_mul_ps(val, val), weight);
+
+			sum = _mm256_add_ps(sum, res);
 		}
 
-		l2[i] = sum;
+		__m128 sum_128 = _mm_add_ps(_mm256_castps256_ps128(sum), _mm256_extractf128_ps(sum, 1));
+		sum_128 = _mm_add_ps(sum_128, _mm_movehdup_ps(sum_128));
+		sum_128 = _mm_add_ps(sum_128, _mm_movehl_ps(sum_128, sum_128));
+
+		l2[i] = _mm_cvtss_f32(sum_128) + net.l2_biases[nbucket][i];
 	}
 
-	float sum = net.output_biases[nbucket];
+	float score = net.output_biases[nbucket];
 	for (int i = 0; i < L2_SIZE; i++) {
 		float val = l2[i];
 		val = std::clamp(val, 0.0f, 1.0f);
 
 		float weight = net.output_weights[nbucket][i];
 
-		sum += val * val * weight;
+		score += val * val * weight;
 	}
 
-	return roundf(sum * SCALE);
+	return roundf(score * SCALE);
 }
