de28a1a0c36a6de1c76f6ae5c2b1040bf2feed20
Kevin Lu <kevie.lu8@gmail.com>
2026-06-22 15:01:55 -0700

    SPSA
    Bench: 3118259

--- engine/params.hpp
+++ engine/params.hpp
@@ -22,7 +22,7 @@
 #include <string>
 #include <list>
 
-// #define TUNING
+#define TUNING
 
 struct TunableParam {
     std::string name;
@@ -57,25 +57,24 @@ TUNE(qs_see, -29, -100, 100, 10);
 TUNE(qsfp_margin, 241, 100, 500, 25);
 TUNE(rfp_threshold, 71, 20, 110, 10);
 TUNE(rfp_improving, 45, 10, 50, 8);
-TUNE(rfp_quad, 7, 1, 8, 1);
+TUNE(rfp_quad, 224, 1, 320, 16); // quantized to 32
 TUNE(rfp_cutnode, 30, 5, 30, 5);
 TUNE(rfp_corr, 112, 20, 250, 20);
-TUNE(nmp_margin, 184, 50, 400, 25);
-TUNE(nmp_depth, 8, 0, 40, 5);
+TUNE(nmp_margin, 184, 50, 400, 20);
+TUNE(nmp_depth, 8, 0, 25, 3);
 TUNE(razor_margin, 231, 150, 400, 20);
 TUNE(probcut_margin, 409, 200, 500, 30);
-TUNE(se_base, 6, 1, 10, 1);
+TUNE(se_base, 48, 1, 160, 16); // quantized to 32
 TUNE(dext_base, 26, 10, 50, 5);
-TUNE(dext_capt, 10, -100, 100, 10);
-TUNE(dext_pv, 35, -100, 100, 10);
-TUNE(dext_improving, 7, -50, 50, 10);
+TUNE(dext_capt, 10, 0, 100, 5);
+TUNE(dext_pv, 35, 0, 100, 5);
 TUNE(text_base, 118, 50, 200, 10);
 TUNE(text_capt, 50, 0, 200, 10);
 TUNE(fp_const, 297, 100, 500, 30);
 TUNE(fp_depth, 72, 20, 200, 10);
 TUNE(fp_hist, 32, 10, 64, 5);
 TUNE(history_margin, 2957, 1000, 4000, 256);
-TUNE(see_quad, 17, 10, 40, 4);
+TUNE(see_quad, 17, 0, 40, 4);
 TUNE(see_lin, 58, 20, 80, 6);
 TUNE(lmr_base, -72, -1024, 1024, 160);
 TUNE(lmr_capt, 57, 0, 128, 16);
@@ -85,18 +84,18 @@ TUNE(lmr_cutnode_nott, -653, -1024, 1024, 256);
 TUNE(lmr_cutoffcnt, 561, 0, 2048, 128);
 TUNE(lmr_ttpv, 834, 0, 2048, 128);
 TUNE(lmr_killer, 974, 0, 2048, 128);
-TUNE(lmr_hist, 12, 1, 40, 2);
+TUNE(lmr_hist, 12, 1, 40, 4);
 TUNE(lmr_ttcapt, 754, 0, 2048, 128);
 TUNE(lmr_ttpv_alpha, 70, -1024, 1024, 256);
 TUNE(lmr_corr, 456, 0, 1024, 128);
 TUNE(dodeeper_margin, 109, 20, 200, 10);
 TUNE(doshallower_margin, 25, -64, 64, 8);
 TUNE(hist_large_margin, 101, 0, 256, 16);
-TUNE(hist_quad, 4, 0, 6, 1);
-TUNE(hist_lin, 139, 64, 256, 15);
+TUNE(hist_quad, 128, 0, 224, 16); // quantized to 32
+TUNE(hist_lin, 139, 64, 256, 16);
 TUNE(hist_const, 124, 0, 256, 16);
-TUNE(postlmr_quad, 3, 0, 6, 1);
-TUNE(postlmr_lin, 126, 64, 256, 15);
+TUNE(postlmr_quad, 96, 0, 224, 16); // quantized to 32
+TUNE(postlmr_lin, 126, 64, 256, 16);
 TUNE(postlmr_const, 112, 0, 256, 16);
 TUNE(asp_window, 13, 1, 30, 4);
 TUNE(corr_ps, 157, 64, 256, 12);
@@ -105,8 +104,8 @@ TUNE(corr_maj, 74, 32, 128, 8);
 TUNE(corr_min, 78, 32, 128, 8);
 TUNE(corr_cont, 150, 64, 256, 16);
 TUNE(corr_cont2, 126, 64, 256, 16);
-TUNE(corr_threat, 90, 64, 256, 16);
-TUNE(probcut_see, 62, 50, 200, 10);
+TUNE(corr_threat, 90, 64, 256, 8);
+TUNE(probcut_see, 62, 0, 200, 10);
 TUNE(badnoisy_div, 47, 10, 100, 8);
 
 TUNE(lmr_a, 99, 20, 200, 10);
