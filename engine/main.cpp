d4b2d38ddd7a0285871a8b780ee2775e12f5bfc6
William Ma <wdotmathree@gmail.com>
2026-06-25 00:20:42 -0400

    Add SPSA values after 30k games (#349)
    ```
    Elo   | 1.70 +- 3.30 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=128MB
    LLR   | 0.41 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 10200 W: 2405 L: 2355 D: 5440
    Penta | [8, 1157, 2727, 1193, 15]
    ```
    https://ob.int0x80.ca/test/946/
    
    Bench: 2458548
    
    ---------
    
    Co-authored-by: Kevin Lu <kevie.lu8@gmail.com>

--- engine/main.cpp
+++ engine/main.cpp
@@ -26,7 +26,6 @@
 #include "eval.hpp"
 #include "history.hpp"
 #include "movegen.hpp"
-#include "movetimings.hpp"
 #include "search.hpp"
 #include "threads.hpp"
 #include "ttable.hpp"
@@ -38,6 +37,12 @@
 size_t TT_SIZE = DEFAULT_TT_SIZE;
 bool quiet = false, online = false, dfrc_uci = false;
 int move_overhead = 0;
+
+uint64_t timemgmt(int64_t remtime, int64_t inc = 0, bool online = 0) {
+	// Return time in ms that we can spend on this move
+	if (online && remtime < 5000) return 100;
+	return std::max(1ll, (long long)(remtime * (tm_rem() / 100.0) + inc * (tm_inc() / 100.0)));
+}
 
 void run_uci() {
 	Pool pool;
