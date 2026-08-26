d639f3903e0a1d7395bd3285fa581a86079464cc
Kevin Lu <kevie.lu8@gmail.com>
2026-08-26 13:43:00 -0700

    Fix max function
    Bench: 2120234

--- engine/main.cpp
+++ engine/main.cpp
@@ -38,9 +38,9 @@ size_t TT_SIZE = DEFAULT_TT_SIZE;
 bool quiet = false, dfrc_uci = false;
 int move_overhead = 0;
 
-uint64_t timemgmt(int64_t remtime, int64_t inc = 0) {
+int64_t timemgmt(int64_t remtime, int64_t inc = 0) {
 	// Return time in ms that we can spend on this move
-	uint64_t x = std::max(1ll, (long long)(remtime * (tm_rem() / 100.0) + inc * (tm_inc() / 100.0)));
+	int64_t x = std::max(1ll, (long long)(remtime * (tm_rem() / 100.0) + inc * (tm_inc() / 100.0)));
 	x = std::min(x, remtime - move_overhead); // prevent situations with large inc but low time where we lose on time
 	return x;
 }
